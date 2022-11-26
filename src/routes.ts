f73f5b6bf6ef1a85468c622619c35e917f738160
eriley <me@eoghanriley.dev>
2022-11-26 15:58:38 -0500

    Added saves route and new stores

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import Hunt from './components/hunt.svelte';
 import Settings from './components/settings.svelte';
+import Save from './components/settings.svelte';
 
 export const routes = {
   '/': Hunt,
-  '/settings': Settings
+  '/settings': Settings,
+  '/saves': Save
 };
--- src/stores.ts
+++ src/stores.ts
@@ -1,5 +1,7 @@
 import { writable } from 'svelte/store';
+import { get } from 'svelte/store';
 
+// Settings
 let storedSettings = localStorage.settings;
 if (storedSettings !== undefined) {
   storedSettings = JSON.parse(storedSettings);
@@ -25,3 +27,24 @@ export const Settings = writable(
 );
 
 Settings.subscribe((value) => (localStorage.settings = JSON.stringify(value)));
+
+// Used in hunt to store active hunt data
+export const ActiveHunt = writable({
+  pokemon: 'bulbasaur',
+  count: 0
+});
+
+// Used to store all hunts
+let storedHunts = localStorage.hunts;
+if (storedHunts !== undefined) {
+  storedHunts = JSON.parse(storedHunts);
+}
+
+export const Hunts = writable(
+  storedHunts ||
+  ActiveHunt.subscribe((value) => {
+    return value;
+  })
+);
+
+Hunts.subscribe((value) => (localStorage.hunts = JSON.stringify(value)));
